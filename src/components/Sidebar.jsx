1def9f1076dceb7b92059f3e5683015cf1b2014d
Attah David <jocheattahdavid@gmail.com>
2020-09-17 16:25:15 +0100

    sidebar header & search ui

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,38 @@
-import React from 'react';
-import './Sidebar.css';
+import React from "react";
+import "./Sidebar.css";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { Avatar, IconButton } from "@material-ui/core";
+// import { SearchOutlined } from '@material-ui/icons';
+import SearchIcon from '@material-ui/icons/Search';
 
 function Sidebar() {
-    return (
-        <div className="sidebar">
-        <h1> sidebar </h1>
-            
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar src="https://avatars3.githubusercontent.com/u/39128641?s=460&u=fc6522969d033a4ed02f00ec1f56ffee419cbad2&v=4" />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
         </div>
-    )
+      </div>
+      <div className="sidebar__search">
+          <div className="sidebar__searchContainer">
+              {/* <SearchOutlined /> */}
+              <SearchIcon />
+              <input type="text" placeholder="Search or Start a new Chat"/>
+          </div>
+      </div>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
