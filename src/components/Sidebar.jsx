17998e7db04b35b4688fe89b52068a939bb52897
Attah David <jocheattahdavid@gmail.com>
2020-09-20 21:42:58 +0100

    sidebarChat started

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./Sidebar.css";
+
+import SidebarChat from "./SidebarChat";
+
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import { Avatar, IconButton } from "@material-ui/core";
 // import { SearchOutlined } from '@material-ui/icons';
 import SearchIcon from '@material-ui/icons/Search';
+
 
 function Sidebar() {
   return (
@@ -27,9 +31,16 @@ function Sidebar() {
       <div className="sidebar__search">
           <div className="sidebar__searchContainer">
               {/* <SearchOutlined /> */}
-              <SearchIcon />
+              <SearchIcon className="sidebar__searchIcon" />
               <input type="text" placeholder="Search or Start a new Chat"/>
           </div>
+      </div>
+      <div className="sidebar__chats">
+      
+          <SidebarChat />
+          <SidebarChat />
+          <SidebarChat />
+          <SidebarChat />
       </div>
     </div>
   );
