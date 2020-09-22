8ca93c4c0bcb336bc94179ca7521f40435600b34
Attah David <jocheattahdavid@gmail.com>
2020-09-22 13:08:46 +0100

    chat header complete

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,10 +1,35 @@
 import React from "react";
 import "./Chat.css";
 
+import { Avatar, IconButton } from "@material-ui/core";
+import { AttachFile, MoreVert } from "@material-ui/icons";
+import SearchIcon from '@material-ui/icons/Search';
+
+
 function Chat() {
   return (
     <div className="chat">
-      <h1>Chat</h1>
+      <div className="chat__header">
+        <Avatar className="chat__avatar"/>
+
+        <div className="chat__info">
+          <h3 className="chat__name">Room name</h3>
+          <p className="chat__lastSeen">Last seen at...</p>
+        </div>
+
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchIcon/>
+          </IconButton>
+          <IconButton>
+            <AttachFile/>
+          </IconButton>
+          <IconButton>
+            <MoreVert/>
+          </IconButton>
+        </div>
+      
+      </div>
     </div>
   );
 }
